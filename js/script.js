5be99894797ce9d9823c4399976dcf349eb92a0b
Karan <sharma.karan27oct@gmail.com>
2021-11-16 21:09:51 +0530

    swiper added

--- js/script.js
+++ js/script.js
@@ -85,3 +85,19 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+// PORTFOLIO SECTION 
+
+
+let swiper = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
